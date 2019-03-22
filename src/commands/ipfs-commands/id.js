8affa095b5ab2c870fc9e5cab5c38630c1dbfc34
JGAntunes <me@jgantunes.com>
2019-03-22 01:42:08 +0000

    feat: add ipfs id command

--- src/commands/ipfs-commands/id.js
+++ src/commands/ipfs-commands/id.js
@@ -7,8 +7,8 @@ const { getRandomElement } = require('../../lib/utils')
 const k8sClient = require('../../lib/kubernetes-client')
 
 const cmd = {
-  command: 'swarm peers [node-id]',
-  desc: 'execute a get swarm peers from [node-id] or a random node',
+  command: 'id [node-id]',
+  desc: 'execute a get id from [node-id] or a random node',
   builder: (yargs) => {
     yargs.positional('node-id', {
       describe: 'node to execute the command at',
@@ -21,8 +21,8 @@ const cmd = {
     if (!node) return
     const ipfs = ipfsClient(node.hosts.ipfsAPI)
     console.log({ name: node.name, id: node.id })
-    const peers = await ipfs.swarm.peers()
-    console.log(peers)
+    const id = await ipfs.id()
+    console.log(id)
   }
 }
 
